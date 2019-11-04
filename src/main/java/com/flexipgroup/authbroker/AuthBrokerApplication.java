9b35aae90142671166ddbba54dff9bcaaacb0300
Saidi Adegoke <rasheedsaidi@gmail.com>
2019-11-04 11:12:20 +0100

    Start managing project with Git

---
+++ src/main/java/com/flexipgroup/authbroker/AuthBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.flexipgroup.authbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBrokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexipgroup/authbroker/AuthBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexipgroup.authbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
